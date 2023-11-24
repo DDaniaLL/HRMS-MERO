cf6c16c3ca9302d73fea8c83dee5d62426558f2a
DDaniall <dany.janboura@gmail.com>
2023-11-24 21:46:04 +0300

    coplied holiday model and migration

--- database/migrations/2023_11_24_181328_create_holidays_table.php
+++ database/migrations/2023_11_24_181328_create_holidays_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('holidays', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->string('year')->nullable();
+            $table->string('path')->nullable();
             $table->timestamps();
         });
     }

d37c6f8f1631336c6d5aff2352bd1f0a7062d281
DDaniall <dany.janboura@gmail.com>
2023-11-24 21:47:25 +0300

    copied comlist model and migration

--- app/Models/Comlist.php
+++ app/Models/Comlist.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Comlist extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'hours',
+        'status',
+        'overtime_id',
+        'expired_date',
+    ];
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function overtime()
+    {
+        return $this->belongsTo(Overtime::class);
+    }
+
 }
--- database/migrations/2023_11_24_181340_create_comlists_table.php
+++ database/migrations/2023_11_24_181340_create_comlists_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('comlists', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->nullable();
+            $table->string('hours')->nullable();
+            $table->foreignId('overtime_id')->nullable();
+            $table->string('status')->nullable()->default('Active');
+            $table->date('expired_date')->nullable();
+            $table->string('autodate')->nullable();
+            $table->string('expired_value')->nullable();
             $table->timestamps();
         });
     }

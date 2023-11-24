136b74f914cb36cc24c8d2a4c42ffd9ad8b8083d
DDaniall <dany.janboura@gmail.com>
2023-11-24 21:31:56 +0300

    copied overtime model and migration

--- app/Models/Overtime.php
+++ app/Models/Overtime.php
@@ -4,8 +4,38 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Overtime extends Model
 {
     use HasFactory;
+    use LogsActivity;
+    use SoftDeletes;
+
+    protected static $recordEvents = ['deleted'];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['status', 'deleted_at', 'user.name']);
+    }
+
+    protected $fillable = [
+        'date',
+        'start_hour',
+        'end_hour',
+        'reason', 
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comlists()
+    {
+        return $this->hasMany(Comlist::class);
+    }
 }
--- database/migrations/2023_11_24_181250_create_overtimes_table.php
+++ database/migrations/2023_11_24_181250_create_overtimes_table.php
@@ -13,6 +13,27 @@ return new class extends Migration
     {
         Schema::create('overtimes', function (Blueprint $table) {
             $table->id();
+            $table->string('status')->nullable();
+            $table->string('type')->nullable();
+            $table->date('date')->nullable();
+            $table->time('start_hour')->nullable();
+            $table->time('end_hour')->nullable();
+            $table->string('hours')->nullable();
+            $table->string('reason')->nullable();
+            $table->string('lmapprover')->nullable();
+            $table->string('lmcomment')->nullable();
+            $table->dateTime('lmdate')->nullable();
+            $table->string('hrapprover')->nullable();
+            $table->string('hrcomment')->nullable();
+            $table->dateTime('hrdate')->nullable();
+            $table->string('exapprover')->nullable();
+            $table->string('excomment')->nullable();
+            $table->dateTime('exdate')->nullable();
+            $table->string('path')->nullable();
+            $table->string('value')->nullable();
+            $table->date('cancelled_date')->nullable();
+            $table->softDeletes();
+            $table->foreignId('user_id');
             $table->timestamps();
         });
     }

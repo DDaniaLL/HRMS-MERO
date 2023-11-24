a596f4dd35d87e27647fb8f72bf1e51d24e2e2c3
DDaniall <dany.janboura@gmail.com>
2023-11-24 21:26:59 +0300

    copied user model and migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,25 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->date('birth_date')->nullable();
+            $table->string('employee_number')->unique()->nullable();
+            $table->string('contract')->nullable();
+            $table->string('position')->nullable();
+            $table->string('office')->nullable();
+            $table->string('department')->nullable();
+            $table->string('linemanager')->nullable();
+            $table->string('isprob')->nullable();
+            $table->string('hradmin')->nullable();
+            $table->string('superadmin')->nullable();
+            $table->date('joined_date')->nullable();
+            $table->date('resigned_date')->nullable();
+            $table->string('status')->nullable()->default('active');
+            $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->text('token')->nullable();
+            $table->foreignId('usertype_id')->nullable();
+            $table->string('preflang')->nullable()->default('en');
             $table->rememberToken();
             $table->timestamps();
         });

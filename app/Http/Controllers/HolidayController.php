abf94d9cc5f396fed28a9082b2c35bbb4e22bda7
DDaniall <dany.janboura@gmail.com>
2023-11-25 15:41:52 +0300

    copied holiday and policy controller

--- app/Http/Controllers/HolidayController.php
+++ app/Http/Controllers/HolidayController.php
@@ -4,62 +4,94 @@ namespace App\Http\Controllers;
 
 use App\Models\Holiday;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HolidayController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+   
     public function index()
     {
-        //
+        $holiday = Holiday::all();
+        return view('admin.holidays.index', ['holidays' => $holiday]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+    
     public function create()
     {
-        //
+        $authuser = Auth::user();
+        if ($authuser->hradmin == "yes")
+        {
+            return view('admin.holidays.create');
+        }
+        else
+        {
+            abort(403);
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    
     public function store(Request $request)
     {
-        //
+        $authuser = Auth::user();
+        if ($authuser->hradmin !== "yes")
+        {
+            abort(403);
+        }
+        else
+        {
+            $request->validate([
+                'name' => 'required|regex:/^[\pL\s]+$/u|min:3|unique:holidays,name',
+                'year' => 'required',
+                'file' => 'required|mimes:jpeg,png,jpg,pdf|max:3072',
+            ],[
+                'name.regex' => trans('overtimeerror.regex'), // custom message
+            ]);    
+            $path = $request->file('file')->storeAs('public/files', $request->name . '.pdf');
+
+            $holiday = new Holiday();
+            $holiday->name = $request->name;
+            $holiday->year = $request->year;
+    
+            $holiday->path = $path;
+            $holiday->save();
+    
+            $holiday = Holiday::all();
+            return view('admin.holidays.index', ['holidays' => $holiday]);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
+   
     public function show(Holiday $holiday)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+   
     public function edit(Holiday $holiday)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+   
     public function update(Request $request, Holiday $holiday)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+    
     public function destroy(Holiday $holiday)
     {
-        //
+        $authuser = Auth::user();
+        if ($authuser->hradmin == "yes")
+        {
+            $file_path = public_path() . '/storage/files/' . $holiday->name . '.pdf';
+            unlink($file_path);
+            $holiday->delete();
+            return redirect()->route('admin.holidays.index');
+        }
+        else
+        {
+            abort(403);
+        }
     }
 }
--- app/Http/Controllers/PolicyController.php
+++ app/Http/Controllers/PolicyController.php
@@ -4,62 +4,98 @@ namespace App\Http\Controllers;
 
 use App\Models\Policy;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PolicyController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    
     public function index()
     {
-        //
+        $policy = Policy::all();
+        return view('admin.policies.index', ['policies' => $policy]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
+        $authuser = Auth::user();
+        if ($authuser->hradmin == "yes")
+        {
+            return view('admin.policies.create');
+        }
+        else
+        {
+            abort(403);
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $authuser = Auth::user();
+        if ($authuser->hradmin !== "yes")
+        {
+            abort(403);
+        }
+        else
+        {
+            $request->validate([
+
+                'name' => 'required|regex:/^[\pL\s]+$/u|min:3|unique:policies,name',
+                'desc',
+                'created_date' => 'required',
+                'lastupdate_date' => 'required',
+                'file' => 'required|mimes:jpeg,png,jpg,pdf|max:3072',
+            ],[
+                'name.regex' => trans('overtimeerror.regex'), // custom message
+            ]);
+    
+            $path = $request->file('file')->storeAs('public/files', $request->name . '.pdf');
+    
+            $policy = new Policy();
+            $policy->name = $request->name;
+            $policy->desc = $request->desc;
+            $policy->created_date = $request->created_date;
+            $policy->lastupdate_date = $request->lastupdate_date;
+            $policy->path = $path;
+            $policy->save();
+    
+            $policy = Policy::all();
+            return view('admin.policies.index', ['policies' => $policy]);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Policy $policy)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+   
     public function edit(Policy $policy)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+    
     public function update(Request $request, Policy $policy)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+   
     public function destroy(Policy $policy)
     {
-        //
+        $authuser = Auth::user();
+        if ($authuser->hradmin == "yes")
+        {
+             // File::delete($policy->name);
+             // File::delete(public_path("files/{{$policy->name}} . '.pdf'"));
+          $file_path = public_path() . '/storage/files/' . $policy->name . '.pdf';
+          unlink($file_path);
+          $policy->delete();
+          return redirect()->route('admin.policies.index');
+        }
+        else
+        {
+            abort(403);
+        }
     }
 }
